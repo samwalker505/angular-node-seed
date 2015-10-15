fecf2d9ce813e05441ea4ecf66fa65c912da5464
samwalker <samwalker505@gmail.com>
2015-10-15 11:25:26 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/partials/:filename', function (req, res) {
+  var filename = req.params.filename;
+  if(!filename) return;  // might want to change this
+  res.render("partials/" + filename );
+};
+
+module.exports = router;
